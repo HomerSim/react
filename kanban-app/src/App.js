e1718823dee0453cae7ebb8f96583fe610f0bf0a
HomerSim <wowls5000@naver.com>
2020-01-28 23:52:34 +0900

    react 칸반앱 튜토리얼

---
+++ kanban-app/src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import KanbanBoard from './KanbanBoard';
+
+let cardsList = [
+  {
+    id : 1,
+    title : 'Read the Book',
+    description : 'I should read the whole book',
+    status : 'in-progress',
+    tasks : []
+  },
+  {
+    id : 2,
+    title : 'Write som code',
+    description : 'Code along with the samples in the book',
+    status : 'todo',
+    tasks : [
+      {
+        id : 1,
+        name : 'ContactList Example',
+        done: true
+      },
+      {
+        id : 2,
+        name : 'Kanban Example',
+        done: false
+      },
+      {
+        id : 3,
+        name : 'My own experiments',
+        done: false
+      }
+    ]
+  },
+];
+
+class App extends Component {
+  render () {
+    return (
+      <KanbanBoard cards={cardsList}>Hello ~ Homersim</KanbanBoard>
+    );
+  }
+}
+
+
+export default App;
---
+++ kanban-app/src/KanbanBoard.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import List from './List';
+
+class KanbanBoard extends Component {
+    render () {
+      return (
+        <div className="app">
+            <List id="todo" title="TO DO" cards={
+                this.props.cards.filter((card) => card.status === "todo")
+            }></List>
+
+            <List id="in-progress" title="In Progress" cards={
+                this.props.cards.filter((card) => card.status === "in-progress")
+            }></List>
+
+            <List id="done" title="Done" cards={
+                this.props.cards.filter((card) => card.status === "done")
+            }></List>
+        </div>
+      );
+    }
+  }
+  
+  export default KanbanBoard;
