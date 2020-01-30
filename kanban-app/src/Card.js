015d29fe9dcb536822282effa22b37d162f9c2b8
HomerSim <wowls5000@naver.com>
2020-01-30 23:48:08 +0900

    method bind 기능 사용해봄

--- kanban-app/src/Card.js
+++ kanban-app/src/Card.js
@@ -8,6 +8,14 @@ class Card extends Component {
             showDetails:false
         }
     }
+
+    toggleDetails() {
+        this.setState(
+            {
+                showDetails:!this.state.showDetails
+            }
+        );
+    }
     render () {
         let cardDetails;
         if (this.state.showDetails) {
@@ -20,8 +28,9 @@ class Card extends Component {
 
         }
       return (
+        
         <div className="card">
-            <div className="card__title" onClick={() => this.setState({showDetails: !this.state.showDetails})}>{this.props.title}</div>
+            <div className={this.state.showDetails?"card__title card__title--is_open":"card__title"} onClick={this.toggleDetails.bind(this)}>{this.props.title}</div>
             {cardDetails}
         </div>
       );
